82e9aeb86243efab765770007e2df7ff27d7d70d
ebrofi <ebrofi@gmail.com>
2023-06-25 15:26:56 +0200

    added proper serializer for workouttemplate

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from data.models import Item, UserData, WorkoutTemplate
+from data.models import Item, UserData, WorkoutTemplate, ExerciseTemplate
 
 # only testing
 class ItemSerializer(serializers.ModelSerializer):
@@ -15,7 +15,26 @@ class UserDataSerializer(serializers.ModelSerializer):
       
 # Workout serializers
 # ONLY FOR TESTING, needs to be changes to proper serializer
-class WorkoutTemplateSerializer(serializers.ModelSerializer):
+class ExerciseTemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseTemplate
+        fields = ['exercise']
+
+class WorkoutTemplateSerializer(serializers.ModelSerializer):    
+    
+    exercises = ExerciseTemplateSerializer(many=True)
+        
     class Meta:
         model = WorkoutTemplate
-        fields = "__all__"
+        fields = ['id', 'workoutTitle', 'exercises']
+    
+    def create(self, validated_data):
+        exercises_data = validated_data.pop('exercises')
+        workout_template = WorkoutTemplate.objects.create(**validated_data)
+        
+        for exercise_data in exercises_data:
+            exercise_serializer = ExerciseTemplateSerializer(data=exercise_data)
+            exercise_serializer.is_valid(raise_exception=True)
+            ExerciseTemplate.objects.create(workoutTemplate=workout_template, **exercise_serializer.validated_data)
+        
+        return workout_template
